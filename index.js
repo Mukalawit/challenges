af2060f56babd5b169dcc828fa6cb2f1b94dcfb7
mukalawit <mgstn15@gmail.com>
2022-12-15 22:55:19 +0300

    fix: cater for no tickets

--- index.js
+++ index.js
@@ -1,45 +1,45 @@
-function generateTicketCSV(tickets){
-let source = [];
-let fail = 0;
-for(let ticket of tickets){
-
-    //identify the source
-    //iterate the array of tickets to check if source exists anywhere as destination
-    for(let check of tickets){
-        
-        if(ticket['source'] === check['destination']){
-            fail++;
+function generateTicketCSV(tickets) {
+  if (tickets.length) {
+    let source = [];
+    let fail = 0;
+    for (let ticket of tickets) {
+      //identify the source
+      //iterate the array of tickets to check if source exists anywhere as destination
+      for (let check of tickets) {
+        if (ticket['source'] === check['destination']) {
+          fail++;
         }
-    }
-    if(!fail){
+      }
+      if (!fail) {
         source.push(ticket['source']);
-    }
-   
-fail=0;
-}
+      }
 
-if(source.length === 1){
-   //find the next destination after the source
-    for(let ticket of tickets){
-        if(source[source.length-1] === ticket['source']){
-            source.push(ticket['destination']);
+      fail = 0;
+    }
+
+    if (source.length === 1) {
+      //find the next destination after the source
+      for (let ticket of tickets) {
+        if (source[source.length - 1] === ticket['source']) {
+          source.push(ticket['destination']);
         }
+      }
+      console.log(source);
+    } else if (source.length > 1) {
+      console.log('Missing tickets');
+    } else {
+      console.log('Starting point is not defined');
     }
-    console.log(source);
-
-}else if(source.length > 1){
-    console.log('Missing tickets');
-}else{
-    console.log('Starting point is not defined');
+  } else {
+    console.log('No tickets');
+  }
 }
-}
-
 
 const tickets = [
-    {source: 'A', destination: 'B'}, 
-    {source: 'B', destination: 'C'},
-    {source: 'C', destination: 'D'},
-    {source: 'D', destination: 'K'}
-]
+  { source: 'A', destination: 'B' },
+  { source: 'B', destination: 'C' },
+  { source: 'C', destination: 'D' },
+  { source: 'D', destination: 'K' },
+];
 
 generateTicketCSV(tickets);
